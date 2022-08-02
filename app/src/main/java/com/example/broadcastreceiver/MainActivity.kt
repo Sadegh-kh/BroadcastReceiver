b2e2e073a83a8dab15319a559318d842e90274c9
Sadegh <Sadeghkhoshbayan1@gmail.com>
2022-08-02 07:05:24 +0430

    init network checking with broadcastReceiver and intent filter

--- app/src/main/java/com/example/broadcastreceiver/MainActivity.kt
+++ app/src/main/java/com/example/broadcastreceiver/MainActivity.kt
@@ -1,6 +1,9 @@
 package com.example.broadcastreceiver
 
 import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.content.IntentFilter
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.broadcastreceiver.databinding.ActivityMainBinding
@@ -13,6 +16,29 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
 
+        //context registered broadcast receiver
+        broadcastReceiver=object :BroadcastReceiver(){
+            override fun onReceive(p0: Context?, p1: Intent?) {
+                checkInternetConnection()
+            }
+        }
 
+        val intentFilter=IntentFilter("android.net.conn.CONNECTIVITY_CHANGE")
+        registerReceiver(broadcastReceiver,intentFilter)
+
+    }
+
+    private fun checkInternetConnection() {
+        val checkNet=NetworkChecker(this)
+        if(checkNet.isInternetConnected){
+            binding.txtShowConect.text="Connected!"
+        }else{
+            binding.txtShowConect.text="Disconnected!"
+        }
+    }
+
+    override fun onDestroy() {
+        unregisterReceiver(broadcastReceiver)
+        super.onDestroy()
     }
 }
